81ea644df427d908e877d6be9e9761490a129e85
tariqahmaad <tariq_muzamil@live.com>
2025-03-21 03:47:47 +0300

    Remove unused components and Firebase security rules, update Expo version in package files

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, SafeAreaView } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
 import React from 'react';
 import IndexPage from './Navigators/IndexPage';
 
@@ -8,21 +8,9 @@ export default function App() {
       <View style={{ flex: 1 }}>
         <IndexPage />
       </View>
-      <View style={{ flex: 1, backgroundColor: '#f0f0f0' }}>
-        <Text style={{ textAlign: 'center', padding: 20 }}>Footer</Text>
-      </View>
     </SafeAreaView>
-
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
 
 
 
