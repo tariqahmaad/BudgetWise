e48d5ff8b5a96a5c0f6fba40c632d7ee7aab2fcb
Mohammad Rauf <mdxrauf@gmail.com>
2025-03-23 23:55:34 +0300

    Refactor font imports and default styles in IndexPage; update navigation options for onboarding screen

--- Navigators/IndexPage.js
+++ Navigators/IndexPage.js
@@ -2,8 +2,8 @@ import { StyleSheet, Text, View, TextInput } from "react-native";
 import React, { useCallback } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 import LoginPage from "../Screens/Authenication/LoginPage";
 import SignUpPage from "../Screens/Authenication/SignUpPage";
@@ -18,34 +18,34 @@ SplashScreen.preventAutoHideAsync();
 
 export default function IndexPage() {
   const [fontsLoaded] = Font.useFonts({
-    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
-    'Poppins-BlackItalic': require('../assets/fonts/Poppins-BlackItalic.ttf'),
-    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
-    'Poppins-BoldItalic': require('../assets/fonts/Poppins-BoldItalic.ttf'),
-    'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
-    'Poppins-ExtraBoldItalic': require('../assets/fonts/Poppins-ExtraBoldItalic.ttf'),
-    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
-    'Poppins-ExtraLightItalic': require('../assets/fonts/Poppins-ExtraLightItalic.ttf'),
-    'Poppins-Italic': require('../assets/fonts/Poppins-Italic.ttf'),
-    'Poppins-Light': require('../assets/fonts/Poppins-Light.ttf'),
-    'Poppins-LightItalic': require('../assets/fonts/Poppins-LightItalic.ttf'),
-    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
-    'Poppins-MediumItalic': require('../assets/fonts/Poppins-MediumItalic.ttf'),
-    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
-    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
-    'Poppins-SemiBoldItalic': require('../assets/fonts/Poppins-SemiBoldItalic.ttf'),
-    'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf'),
-    'Poppins-ThinItalic': require('../assets/fonts/Poppins-ThinItalic.ttf'),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-BlackItalic": require("../assets/fonts/Poppins-BlackItalic.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-BoldItalic": require("../assets/fonts/Poppins-BoldItalic.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraBoldItalic": require("../assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-ExtraLightItalic": require("../assets/fonts/Poppins-ExtraLightItalic.ttf"),
+    "Poppins-Italic": require("../assets/fonts/Poppins-Italic.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-LightItalic": require("../assets/fonts/Poppins-LightItalic.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-MediumItalic": require("../assets/fonts/Poppins-MediumItalic.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-SemiBoldItalic": require("../assets/fonts/Poppins-SemiBoldItalic.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-ThinItalic": require("../assets/fonts/Poppins-ThinItalic.ttf"),
   });
-  
+
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
       // Set Poppins as default for all Text and TextInput components
       Text.defaultProps = Text.defaultProps || {};
-      Text.defaultProps.style = { fontFamily: 'Poppins-Regular' };
+      Text.defaultProps.style = { fontFamily: "Poppins-Regular" };
 
       TextInput.defaultProps = TextInput.defaultProps || {};
-      TextInput.defaultProps.style = { fontFamily: 'Poppins-Regular' };
+      TextInput.defaultProps.style = { fontFamily: "Poppins-Regular" };
 
       await SplashScreen.hideAsync();
     }
@@ -59,7 +59,11 @@ export default function IndexPage() {
     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Authentication">
-          <Stack.Screen name="onboarding" component={OnboardingScreen} />
+          <Stack.Screen
+            name="onboarding"
+            component={OnboardingScreen}
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name="Login" component={LoginPage} />
           <Stack.Screen name="SignUp" component={SignUpPage} />
           <Stack.Screen name="Forgot" component={ForgotPage} />
